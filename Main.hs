d971fadd5559dfe35c515c71b3a190823f3abb2a
Andrii Ivanov <ANDIvanov@luxoft.com>
2018-03-15 18:27:53 +0200

    Update

--- Main.hs
+++ Main.hs
@@ -1,7 +1,24 @@
 module Main where
 
 main :: IO ()
-main = putStrLn (show (double 2))
+main = print (findById(1) :: Maybe User)
 
-double :: Int -> Int
-double x = x + x
+data User = User Int String String deriving (Eq, Show)
+
+class Repository a where
+  findAll :: [a]
+  findById :: Int -> Maybe a
+
+class SafeList a where
+  safeHead :: [a] -> Maybe a
+  safeHead [] = Nothing
+  safeHead (x:_) = Just x
+
+instance SafeList User
+
+instance Repository User where
+  findAll = [User 1 "Andrii" "Ivanov", User 2 "Olya" "Ivanova"]
+  findById id = (safeHead . filter(byId id)) findAll
+
+byId :: Int -> User -> Bool
+byId id = \x -> case x of (User uid _ _) -> uid == id
